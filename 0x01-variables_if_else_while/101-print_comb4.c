4603da4f684603196f7220613930ea63d66647e8
Doreen970 <doreenwanyama20@gmail.com>
2023-02-18 05:01:44 -0800

    changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
-						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
@@ -30,4 +30,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
