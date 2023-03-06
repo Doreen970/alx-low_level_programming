a76b464a80be7a7cea6cc4e8242fadd60ac71166
Doreen970 <doreenwanyama20@gmail.com>
2023-03-06 07:56:51 -0800

    function that prints the sum of the two diagonals of a square matrix of integers.

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar - writes the character c to stdout
+* Description: The character to print
+*
+* Return: On success 0
+*/
+void _putchar(char c)
+{
+return (0);
+}
